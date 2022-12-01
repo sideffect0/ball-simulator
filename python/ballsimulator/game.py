3a9fac213d21e57c66c8c786432725f464c4ba85
Renjith Thankachan <sideffect0@Renjiths-MacBook-Air.local>
2022-12-01 19:47:54 +0530

    ecs based ball simulation, add rust

--- python/ballsimulator/game.py
+++ python/ballsimulator/game.py
@@ -1,7 +1,49 @@
-import pygame, sys
+import sys
+import time
+import pygame
 from pygame.locals import QUIT
+from random import randint
 
 game_clock = pygame.time.Clock()
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 600
+BALL_SIZE = 10
+BOUND_X = WINDOW_WIDTH - BALL_SIZE
+BOUND_Y = WINDOW_HEIGHT - BALL_SIZE
+DELTA = 10
+FPS = 60
+
+class Ball(object):
+    def __init__(self):
+        self.pos = (randint(0, WINDOW_WIDTH - BALL_SIZE), randint(0, WINDOW_HEIGHT - BALL_SIZE))
+        self.velX, self.velY = (max(1, randint(-4, 4)), max(1, randint(-4, 4)))
+        self.ball = pygame.image.load('resources/ns-pygame-ball.png');
+
+    def render(self, screen):
+        ball_sf = self.ball.get_rect(center=self.pos)
+        screen.blit(self.ball, ball_sf)
+
+    def move(self):
+        x,y = self.pos
+        if x <= 0 or x >= BOUND_X:
+            self.velX = -self.velX
+        if y <= 0 or y >= BOUND_Y:
+            self.velY = -self.velY
+        self.pos = (x + (DELTA*self.velX), y + (DELTA*self.velY))
+
+class Window(object):
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.bg = pygame.image.load('resources/ns-pygame-bg.jpg')
+        pygame.display.set_caption('Ball Simulator')
+        self.balls = [Ball() for _ in range(5)]
+
+    def render(self):
+        self.screen.blit(self.bg, (0, 0))
+        for ball in self.balls:
+            ball.move()
+            ball.render(self.screen)
 
 def draw_screen(screen, ball_pos):
     bg = pygame.image.load('resources/ns-pygame-bg.jpg')
@@ -14,29 +56,21 @@ def draw_screen(screen, ball_pos):
 
 def init_pygame():
     pygame.init()
-    screen = pygame.display.set_mode((400, 300))
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption('Ball Simulator')
     return screen, draw_screen(screen, (100, 100))
 
 
 def start():
-    screen, (center_x, center_y) = init_pygame()
+    window = Window()
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    center_x -= 10
-                elif event.key == pygame.K_RIGHT:
-                    center_x += 10
-                elif event.key == pygame.K_DOWN:
-                    center_y += 10
-                elif event.key == pygame.K_UP:
-                    center_y -= 10
-                center_x, center_y = draw_screen(screen, (center_x, center_y))
-
+        window.render()         
         pygame.display.update()
-        game_clock.tick(60)
+        game_clock.tick(FPS)
+        # time.sleep(0.1)
 
